59acf65c1ae494ad9b3ed0fb94e119a6832800b0
overflowDT <fedecasares1993@yahoo.com.ar>
2013-10-14 01:21:34 -0300

    Agregado un recorredor de resultados

--- Clinica Frba/Form1.Designer.cs
+++ Clinica Frba/Form1.Designer.cs
@@ -40,7 +40,7 @@ namespace Clinica_Frba
             // 
             this.textBox1.Location = new System.Drawing.Point(66, 87);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
@@ -53,21 +53,21 @@ namespace Clinica_Frba
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(80, 42);
+            this.button1.Location = new System.Drawing.Point(172, 101);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Next";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(95, 190);
+            this.button2.Location = new System.Drawing.Point(91, 196);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
--- Clinica Frba/Form1.cs
+++ Clinica Frba/Form1.cs
@@ -12,9 +12,29 @@ namespace Clinica_Frba
 {
     public partial class Form1 : Form
     {
+        public int cont = 0;
+        public SqlDataReader myReader = null;
         public Form1()
         {
             InitializeComponent();
+            callCon();
+        }
+
+        private void callCon()
+        {
+            SqlConnection con = new SqlConnection("Data Source=localhost\\SQLSERVER2008;Initial Catalog=GD2C2013;User ID=gd;Password=gd2013");
+
+            con.Open();
+            
+            SqlCommand myCommand = new SqlCommand("SELECT top 100  * from GD2C2013.gd_esquema.Maestra",
+                                                     con);
+            myReader = myCommand.ExecuteReader();
+            //while (myReader.Read())
+            //{
+            //    this.textBox1.Text = (myReader[0].ToString());
+            //    this.textBox2.Text = (myReader[1].ToString());
+            //}
+            //con.Close();
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -24,19 +44,11 @@ namespace Clinica_Frba
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=localhost\\SQLSERVER2008;Initial Catalog=GD2C2013;User ID=gd;Password=gd2013");
-
-            con.Open();
-            SqlDataReader myReader = null;
-            SqlCommand myCommand = new SqlCommand("SELECT top 1 * from GD2C2013.gd_esquema.Maestra",
-                                                     con);
-            myReader = myCommand.ExecuteReader();
-            while (myReader.Read())
+            if (myReader.Read())
             {
                 this.textBox1.Text = (myReader[0].ToString());
                 this.textBox2.Text = (myReader[1].ToString());
-            }
-            con.Close();
+            }  
         }
 
         private void button2_Click(object sender, EventArgs e)
