95bf01a7822370956d4c09c1abaf0ca1909f02e1
juanignaciomolina <juanignaciomln@gmail.com>
2013-10-27 19:37:28 -0300

    Se agrega formulario de menu de funcionalidades. Incluye a la función muestraBotones.

---
+++ Clinica Frba/Menu/Menu.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Clinica_Frba.Menu
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_f1_abmAfililiado = new System.Windows.Forms.Button();
+            this.btn_f2_abmProfesional = new System.Windows.Forms.Button();
+            this.btn_f3_abmEspecialidad = new System.Windows.Forms.Button();
+            this.btn_f4_abmPlan = new System.Windows.Forms.Button();
+            this.btn_f6_registrarAgenda = new System.Windows.Forms.Button();
+            this.btn_f7_pedirTurno = new System.Windows.Forms.Button();
+            this.btn_f8_regLlegada = new System.Windows.Forms.Button();
+            this.btn_f9_regResultado = new System.Windows.Forms.Button();
+            this.btn_f5_cancelarAtencion = new System.Windows.Forms.Button();
+            this.btn_f10_generarReceta = new System.Windows.Forms.Button();
+            this.btn_f11_generarEstadisticas = new System.Windows.Forms.Button();
+            this.lbl_menuPrincipal = new System.Windows.Forms.Label();
+            this.btn_f12_comprarBono = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_f1_abmAfililiado
+            // 
+            this.btn_f1_abmAfililiado.Location = new System.Drawing.Point(76, 105);
+            this.btn_f1_abmAfililiado.Name = "btn_f1_abmAfililiado";
+            this.btn_f1_abmAfililiado.Size = new System.Drawing.Size(181, 41);
+            this.btn_f1_abmAfililiado.TabIndex = 0;
+            this.btn_f1_abmAfililiado.Text = "ABM de afiliados";
+            this.btn_f1_abmAfililiado.UseVisualStyleBackColor = true;
+            // 
+            // btn_f2_abmProfesional
+            // 
+            this.btn_f2_abmProfesional.Location = new System.Drawing.Point(76, 152);
+            this.btn_f2_abmProfesional.Name = "btn_f2_abmProfesional";
+            this.btn_f2_abmProfesional.Size = new System.Drawing.Size(181, 41);
+            this.btn_f2_abmProfesional.TabIndex = 1;
+            this.btn_f2_abmProfesional.Text = "ABM de profesionales";
+            this.btn_f2_abmProfesional.UseVisualStyleBackColor = true;
+            this.btn_f2_abmProfesional.Click += new System.EventHandler(this.btn_abmProfesional_Click);
+            // 
+            // btn_f3_abmEspecialidad
+            // 
+            this.btn_f3_abmEspecialidad.Location = new System.Drawing.Point(76, 199);
+            this.btn_f3_abmEspecialidad.Name = "btn_f3_abmEspecialidad";
+            this.btn_f3_abmEspecialidad.Size = new System.Drawing.Size(181, 41);
+            this.btn_f3_abmEspecialidad.TabIndex = 2;
+            this.btn_f3_abmEspecialidad.Text = "ABM de especialidades";
+            this.btn_f3_abmEspecialidad.UseVisualStyleBackColor = true;
+            // 
+            // btn_f4_abmPlan
+            // 
+            this.btn_f4_abmPlan.Location = new System.Drawing.Point(76, 246);
+            this.btn_f4_abmPlan.Name = "btn_f4_abmPlan";
+            this.btn_f4_abmPlan.Size = new System.Drawing.Size(181, 41);
+            this.btn_f4_abmPlan.TabIndex = 3;
+            this.btn_f4_abmPlan.Text = "ABM de planes";
+            this.btn_f4_abmPlan.UseVisualStyleBackColor = true;
+            // 
+            // btn_f6_registrarAgenda
+            // 
+            this.btn_f6_registrarAgenda.Location = new System.Drawing.Point(297, 105);
+            this.btn_f6_registrarAgenda.Name = "btn_f6_registrarAgenda";
+            this.btn_f6_registrarAgenda.Size = new System.Drawing.Size(181, 41);
+            this.btn_f6_registrarAgenda.TabIndex = 4;
+            this.btn_f6_registrarAgenda.Text = "Registrar agenda de profesional";
+            this.btn_f6_registrarAgenda.UseVisualStyleBackColor = true;
+            // 
+            // btn_f7_pedirTurno
+            // 
+            this.btn_f7_pedirTurno.Location = new System.Drawing.Point(297, 152);
+            this.btn_f7_pedirTurno.Name = "btn_f7_pedirTurno";
+            this.btn_f7_pedirTurno.Size = new System.Drawing.Size(181, 41);
+            this.btn_f7_pedirTurno.TabIndex = 5;
+            this.btn_f7_pedirTurno.Text = "Pedir un turno";
+            this.btn_f7_pedirTurno.UseVisualStyleBackColor = true;
+            // 
+            // btn_f8_regLlegada
+            // 
+            this.btn_f8_regLlegada.Location = new System.Drawing.Point(297, 199);
+            this.btn_f8_regLlegada.Name = "btn_f8_regLlegada";
+            this.btn_f8_regLlegada.Size = new System.Drawing.Size(181, 41);
+            this.btn_f8_regLlegada.TabIndex = 6;
+            this.btn_f8_regLlegada.Text = "Registrar llegada";
+            this.btn_f8_regLlegada.UseVisualStyleBackColor = true;
+            // 
+            // btn_f9_regResultado
+            // 
+            this.btn_f9_regResultado.Location = new System.Drawing.Point(297, 246);
+            this.btn_f9_regResultado.Name = "btn_f9_regResultado";
+            this.btn_f9_regResultado.Size = new System.Drawing.Size(181, 41);
+            this.btn_f9_regResultado.TabIndex = 7;
+            this.btn_f9_regResultado.Text = "Registrar resultado de atención";
+            this.btn_f9_regResultado.UseVisualStyleBackColor = true;
+            // 
+            // btn_f5_cancelarAtencion
+            // 
+            this.btn_f5_cancelarAtencion.Location = new System.Drawing.Point(76, 293);
+            this.btn_f5_cancelarAtencion.Name = "btn_f5_cancelarAtencion";
+            this.btn_f5_cancelarAtencion.Size = new System.Drawing.Size(181, 41);
+            this.btn_f5_cancelarAtencion.TabIndex = 8;
+            this.btn_f5_cancelarAtencion.Text = "Cancelar un turno";
+            this.btn_f5_cancelarAtencion.UseVisualStyleBackColor = true;
+            // 
+            // btn_f10_generarReceta
+            // 
+            this.btn_f10_generarReceta.Location = new System.Drawing.Point(297, 293);
+            this.btn_f10_generarReceta.Name = "btn_f10_generarReceta";
+            this.btn_f10_generarReceta.Size = new System.Drawing.Size(181, 41);
+            this.btn_f10_generarReceta.TabIndex = 9;
+            this.btn_f10_generarReceta.Text = "Generar una receta";
+            this.btn_f10_generarReceta.UseVisualStyleBackColor = true;
+            // 
+            // btn_f11_generarEstadisticas
+            // 
+            this.btn_f11_generarEstadisticas.Location = new System.Drawing.Point(76, 340);
+            this.btn_f11_generarEstadisticas.Name = "btn_f11_generarEstadisticas";
+            this.btn_f11_generarEstadisticas.Size = new System.Drawing.Size(181, 41);
+            this.btn_f11_generarEstadisticas.TabIndex = 10;
+            this.btn_f11_generarEstadisticas.Text = "Generar estadísticas";
+            this.btn_f11_generarEstadisticas.UseVisualStyleBackColor = true;
+            // 
+            // lbl_menuPrincipal
+            // 
+            this.lbl_menuPrincipal.AutoSize = true;
+            this.lbl_menuPrincipal.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_menuPrincipal.Location = new System.Drawing.Point(117, 29);
+            this.lbl_menuPrincipal.Name = "lbl_menuPrincipal";
+            this.lbl_menuPrincipal.Size = new System.Drawing.Size(335, 55);
+            this.lbl_menuPrincipal.TabIndex = 11;
+            this.lbl_menuPrincipal.Text = "Menu principal";
+            // 
+            // btn_f12_comprarBono
+            // 
+            this.btn_f12_comprarBono.Location = new System.Drawing.Point(297, 340);
+            this.btn_f12_comprarBono.Name = "btn_f12_comprarBono";
+            this.btn_f12_comprarBono.Size = new System.Drawing.Size(181, 41);
+            this.btn_f12_comprarBono.TabIndex = 12;
+            this.btn_f12_comprarBono.Text = "Comprar un bono";
+            this.btn_f12_comprarBono.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 411);
+            this.Controls.Add(this.btn_f12_comprarBono);
+            this.Controls.Add(this.lbl_menuPrincipal);
+            this.Controls.Add(this.btn_f11_generarEstadisticas);
+            this.Controls.Add(this.btn_f10_generarReceta);
+            this.Controls.Add(this.btn_f5_cancelarAtencion);
+            this.Controls.Add(this.btn_f9_regResultado);
+            this.Controls.Add(this.btn_f8_regLlegada);
+            this.Controls.Add(this.btn_f7_pedirTurno);
+            this.Controls.Add(this.btn_f6_registrarAgenda);
+            this.Controls.Add(this.btn_f4_abmPlan);
+            this.Controls.Add(this.btn_f3_abmEspecialidad);
+            this.Controls.Add(this.btn_f2_abmProfesional);
+            this.Controls.Add(this.btn_f1_abmAfililiado);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_f1_abmAfililiado;
+        private System.Windows.Forms.Button btn_f2_abmProfesional;
+        private System.Windows.Forms.Button btn_f3_abmEspecialidad;
+        private System.Windows.Forms.Button btn_f4_abmPlan;
+        private System.Windows.Forms.Button btn_f6_registrarAgenda;
+        private System.Windows.Forms.Button btn_f7_pedirTurno;
+        private System.Windows.Forms.Button btn_f8_regLlegada;
+        private System.Windows.Forms.Button btn_f9_regResultado;
+        private System.Windows.Forms.Button btn_f5_cancelarAtencion;
+        private System.Windows.Forms.Button btn_f10_generarReceta;
+        private System.Windows.Forms.Button btn_f11_generarEstadisticas;
+        private System.Windows.Forms.Label lbl_menuPrincipal;
+        private System.Windows.Forms.Button btn_f12_comprarBono;
+    }
+}
---
+++ Clinica Frba/Menu/Menu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clinica_Frba.Menu
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+            //Por seguridad, por defecto todos los botones son invisibles
+            btn_f1_abmAfililiado.Visible = false;
+            btn_f2_abmProfesional.Visible = false;
+            btn_f3_abmEspecialidad.Visible = false;
+            btn_f4_abmPlan.Visible = false;
+            btn_f5_cancelarAtencion.Visible = false;
+            btn_f6_registrarAgenda.Visible = false;
+            btn_f7_pedirTurno.Visible = false;
+            btn_f8_regLlegada.Visible = false;
+            btn_f9_regResultado.Visible = false;
+            btn_f10_generarReceta.Visible = false;
+            btn_f11_generarEstadisticas.Visible = false;
+            btn_f12_comprarBono.Visible = false;
+
+
+            //Array de funcionalidades: false = NO, true = SI
+            bool[] funcionalidades;
+            funcionalidades = new bool[12];
+            
+            //Esto es solo para inicializar un array de prueba
+            for (int i=0; i<=11; i++) funcionalidades[i] = true;
+
+            //Se invoca a la función que determinará que botones mostrar
+            //en base al vector de funcionalidades
+            muestraBotones(funcionalidades);
+
+            }
+        
+        //La función muestraBotones recibe 
+        private void muestraBotones (bool[] funcionalidades)
+        {
+
+            //Segun el array de funcionalidades, se mostraran o no los botones.
+            if (funcionalidades[0] == true) btn_f1_abmAfililiado.Visible = true;
+            if (funcionalidades[1] == true) btn_f2_abmProfesional.Visible = true;
+            if (funcionalidades[2] == true) btn_f3_abmEspecialidad.Visible = true;
+            if (funcionalidades[3] == true) btn_f4_abmPlan.Visible = true;
+            if (funcionalidades[4] == true) btn_f5_cancelarAtencion.Visible = true;
+            if (funcionalidades[5] == true) btn_f6_registrarAgenda.Visible = true;
+            if (funcionalidades[6] == true) btn_f7_pedirTurno.Visible = true;
+            if (funcionalidades[7] == true) btn_f8_regLlegada.Visible = true;
+            if (funcionalidades[8] == true) btn_f9_regResultado.Visible = true;
+            if (funcionalidades[9] == true) btn_f10_generarReceta.Visible = true;
+            if (funcionalidades[10] == true) btn_f11_generarEstadisticas.Visible = true;
+            if (funcionalidades[11] == true) btn_f12_comprarBono.Visible = true;
+
+        }
+
+
+        }
+    }
+}
