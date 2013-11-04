e1eb82cde7a43a29604134295ddea0c687708e1a
juanignaciomolina <juanignaciomln@gmail.com>
2013-11-04 00:34:08 -0300

    El form de alta de profesionales ya puede detectar si se trata de ingresar un usuario con el mismo documento.

--- Clinica Frba/Abm de Profesional/frmProfesionalAlta.cs
+++ Clinica Frba/Abm de Profesional/frmProfesionalAlta.cs
@@ -14,6 +14,9 @@ namespace Clinica_Frba.Abm_de_Profesional_Alta
 {
     public partial class frm_ABMpro_Alta : Form
     {
+        //Profesional profesional;
+        SqlRunner runner = new SqlRunner(Properties.Settings.Default.GD2C2013ConnectionString);
+
         public frm_ABMpro_Alta()
         {    
             InitializeComponent();
@@ -35,24 +38,54 @@ namespace Clinica_Frba.Abm_de_Profesional_Alta
                   && cbo_ABMpro_sexo.Text != ""
                   )
             {
-                /*
-                int dni = int.Parse(txt_ABMAfiliado_AltaMod_nrodoc.Text);
-                int telefono = int.Parse(txt_ABMAfiliado_AltaMod_telefono.Text);
-                string nombre_usuario = "u" + Convert.ToString(txt_ABMAfiliado_AltaMod_nrodoc.Text);
-                    
-                    SqlRunner runner = new SqlRunner(Properties.Settings.Default.GD2C2013ConnectionString);
-                    //TODO: parsear datos y terminar el insert
-                    try
+
+                //Convertimos algunos strings en ints para meter en la DB
+                int matricula = int.Parse(txt_ABMpro_matricula.Text);
+                int dni = int.Parse(txt_ABMpro_NDoc.Text);
+                int telefono = int.Parse(txt_ABMpro_telefono.Text);
+
+                string nombre_usuario = "u" + Convert.ToString(txt_ABMpro_NDoc.Text);
+
+                /*TODO: Revisar porque todas las matriculas estan NULL en la DB
+                //Se revisa que la matricula no haya sido asignada previamente
+                var cantidad = runner.Single("SELECT COUNT(*) as ocurrencias FROM SIGKILL.profesional WHERE pro_matricula='{0}'", matricula);
+                if ((int)cantidad["ocurrencias"] != 0)
                     {
-                        var result = runner
-                            .Single("INSERT INTO SIGKILL.Usuario WHERE usr_usuario= '{0}' ", txtUser.Text);
-                    }
-                    catch
-                    {
-                        MessageBox.Show("Error al crear el nuevo afiliado");
+                        MessageBox.Show("La matricula ingresada ya se encuentra asignada a otro profesional.",
+                    "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
                     }
                  */
+
+                //Se revisa que el DNI no haya sido asignado previamente
+                var cantidad = runner.Single("SELECT COUNT(*) as ocurrencias FROM SIGKILL.profesional WHERE pro_dni='{0}'", txt_ABMpro_NDoc.Text);
+                if ((int)cantidad["ocurrencias"] != 0)
+                {
+                    MessageBox.Show("El documento ingresado ya se encuentra asignado a otro profesional.",
+                "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                /*TODO: Formular el INSERT
+                int hab = 0;
+                if (chk_habilitado.Checked) hab = 1;
+
+                runner.Insert("INSERT INTO SIGKILL.rol(rol_nombre,rol_habilitado) VALUES ('{0}',{1})", txtNombre.Text, hab.ToString());
+                var res = runner.Single("SELECT * FROM SIGKILL.rol WHERE rol_nombre='{0}'", txtNombre.Text);
+                Rol newrol = new Adapter().Transform<Rol>(res);
+                foreach (var f in checkedListBox1.CheckedItems)
+                {
+                    runner.Insert("INSERT INTO SIGKILL.func_rol(frol_rol,frol_funcionalidad) VALUES ({0},{1})", newrol.rol_id.ToString(), (checkedListBox1.Items.IndexOf(f) + 1).ToString());
+                }
+               
+                */
+
+                MessageBox.Show("El profesional fue dado de alta satisfactoriamente.",
+                "Operación válida", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+                 
             }
+            //En el caso de que falten ingresar datos, se procedera a informar al usuario
             else
             {
                 MessageBox.Show("Por favor, complete todos los datos para continuar.",
