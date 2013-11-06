31650480532dcaaf72e44213c37d110152374b6d
juanignaciomolina <juanignaciomln@gmail.com>
2013-11-06 11:56:41 -0300

    Se agrega checkeo para revisar si el nuevo profesional tiene usuario

--- Clinica Frba/Abm de Profesional/frmProfesionalAlta.cs
+++ Clinica Frba/Abm de Profesional/frmProfesionalAlta.cs
@@ -38,47 +38,47 @@ namespace Clinica_Frba.Abm_de_Profesional_Alta
                   && cbo_ABMpro_sexo.Text != ""
                   )
             {
-
-                //Convertimos algunos strings en ints para meter en la DB
-                int matricula = int.Parse(txt_ABMpro_matricula.Text);
-                int dni = int.Parse(txt_ABMpro_NDoc.Text);
-                int telefono = int.Parse(txt_ABMpro_telefono.Text);
-
-                string nombre_usuario = "u" + Convert.ToString(txt_ABMpro_NDoc.Text);
-
-                /*TODO: Revisar porque todas las matriculas estan NULL en la DB
+                
+                
                 //Se revisa que la matricula no haya sido asignada previamente
-                var cantidad = runner.Single("SELECT COUNT(*) as ocurrencias FROM SIGKILL.profesional WHERE pro_matricula='{0}'", matricula);
-                if ((int)cantidad["ocurrencias"] != 0)
+                var cantidadMat = runner.Single("SELECT COUNT(*) as ocurrencias FROM SIGKILL.profesional WHERE pro_matricula='{0}'", txt_ABMpro_matricula.Text);
+                if ((int)cantidadMat["ocurrencias"] != 0)
                     {
                         MessageBox.Show("La matricula ingresada ya se encuentra asignada a otro profesional.",
                     "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         return;
                     }
-                 */
+                 
 
                 //Se revisa que el DNI no haya sido asignado previamente
-                var cantidad = runner.Single("SELECT COUNT(*) as ocurrencias FROM SIGKILL.profesional WHERE pro_dni='{0}'", txt_ABMpro_NDoc.Text);
-                if ((int)cantidad["ocurrencias"] != 0)
+                var cantidadDoc = runner.Single("SELECT COUNT(*) as ocurrencias FROM SIGKILL.profesional WHERE pro_dni='{0}'", txt_ABMpro_NDoc.Text);
+                if ((int)cantidadDoc["ocurrencias"] != 0)
                 {
                     MessageBox.Show("El documento ingresado ya se encuentra asignado a otro profesional.",
                 "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return;
                 }
 
-                /*TODO: Formular el INSERT
-                int hab = 0;
-                if (chk_habilitado.Checked) hab = 1;
 
-                runner.Insert("INSERT INTO SIGKILL.rol(rol_nombre,rol_habilitado) VALUES ('{0}',{1})", txtNombre.Text, hab.ToString());
+                //Si llegamos a este punto, ya esta controlado que el profesional no tenga datos
+                //repetidos, por lo que procedemos a crearle un usuario primero si es que no lo tiene
+                string nombre_usuario = "u" + Convert.ToString(txt_ABMpro_NDoc.Text);
+                var existeUsuario = runner.Single("SELECT COUNT(*) as ocurrencias FROM SIGKILL.usuario WHERE usr_usuario='{0}'", nombre_usuario);
+                if ((int)existeUsuario["ocurrencias"] != 0)
+                {
+                    //Si existe un usuario con el mismo documento
+                    MessageBox.Show("el usuario ya exisite");
+                }
+                
+                /*runner.Insert("INSERT INTO SIGKILL.rol(rol_nombre,rol_habilitado) VALUES ('{0}',{1})", txtNombre.Text, hab.ToString());
                 var res = runner.Single("SELECT * FROM SIGKILL.rol WHERE rol_nombre='{0}'", txtNombre.Text);
                 Rol newrol = new Adapter().Transform<Rol>(res);
                 foreach (var f in checkedListBox1.CheckedItems)
                 {
                     runner.Insert("INSERT INTO SIGKILL.func_rol(frol_rol,frol_funcionalidad) VALUES ({0},{1})", newrol.rol_id.ToString(), (checkedListBox1.Items.IndexOf(f) + 1).ToString());
                 }
-               
                 */
+                
 
                 MessageBox.Show("El profesional fue dado de alta satisfactoriamente.",
                 "Operación válida", MessageBoxButtons.OK, MessageBoxIcon.Information);
